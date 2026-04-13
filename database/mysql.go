05d790bc97825c0a45dcb7ba03a65a5856a0f1a1
girly2gether <jasminearta@MacBook-Air-Jasmine.local>
2026-04-14 00:11:33 +0700

    deploy

--- database/mysql.go
+++ database/mysql.go
@@ -3,22 +3,44 @@ package database
 import (
 	"fmt"
 	"log"
+	"os"
 
+	_ "github.com/joho/godotenv/autoload"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
+func getEnv(key, fallback string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return fallback
+	}
+	return value
+}
+
 func ConnectDatabase() {
-	dsn := "root:@tcp(127.0.0.1:3306)/karangtaruna?charset=utf8mb4&parseTime=True&loc=Local"
+	dbHost := getEnv("DB_HOST", "127.0.0.1")
+	dbPort := getEnv("DB_PORT", "3306")
+	dbUser := getEnv("DB_USER", "root")
+	dbPassword := getEnv("DB_PASSWORD", "")
+	dbName := getEnv("DB_NAME", "karangtaruna")
+
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		dbUser,
+		dbPassword,
+		dbHost,
+		dbPort,
+		dbName,
+	)
 
 	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatal("Failed to connect database:", err)
+		log.Fatal("Failed to connect database: ", err)
 	}
 
 	fmt.Println("Database connected!")
-
 	DB = database
 }
