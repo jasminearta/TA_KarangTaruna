30d64da75416bc396b618d404636c327891cc4a3
girly2gether <jasminearta@MacBook-Air-Jasmine.local>
2026-04-20 15:53:07 +0700

    password login

--- internal/entities/user.go
+++ internal/entities/user.go
@@ -4,7 +4,7 @@ type User struct {
 	ID       uint   `gorm:"primaryKey" json:"id"`
 	Nama     string `json:"nama"`
 	Email    string `json:"email"`
-	Password string `json:"-"`
+	Password string `json:"password"`
 	Foto     string `json:"foto"`
 	Role     string `json:"role"`
 	Status   string `json:"status" gorm:"type:varchar(20);default:'aktif'"`
